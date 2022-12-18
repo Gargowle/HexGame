31ee62d3997c86ce2c406af700459ee2f5207ce0
Gargowle <84672530+Gargowle@users.noreply.github.com>
2022-12-18 11:53:22 +0100

    Initial Commit

---
+++ Source/HexGame/HexGameGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "HexGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HEXGAME_API AHexGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
