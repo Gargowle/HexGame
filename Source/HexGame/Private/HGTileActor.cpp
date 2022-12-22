1a8ca299e08fd751e0be7cfce543f6f1d7c37020
Gargowle <84672530+Gargowle@users.noreply.github.com>
2022-12-22 18:56:29 +0100

    Mesh and Axial coordinate members for TileActor

--- Source/HexGame/Private/HGTileActor.cpp
+++ Source/HexGame/Private/HGTileActor.cpp
@@ -3,25 +3,34 @@
 
 #include "HGTileActor.h"
 
+#define Z_POSITION_OFFSET_LIMIT 0.1f
+
 // Sets default values
 AHGTileActor::AHGTileActor()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	MeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("HexMesh"));
+	RootComponent = MeshComponent;
+	
+	// assume default values
+	HexCoord = FAxialCoordinate();
+}
+
+FAxialCoordinate const AHGTileActor::GetHexCoordinate()
+{
+	return HexCoord;
 }
 
 // Called when the game starts or when spawned
 void AHGTileActor::BeginPlay()
 {
 	Super::BeginPlay();
-	
 }
 
-// Called every frame
-void AHGTileActor::Tick(float DeltaTime)
+float AHGTileActor::GetRandomZOffset()
 {
-	Super::Tick(DeltaTime);
-
+	return FMath::RandRange(- Z_POSITION_OFFSET_LIMIT, Z_POSITION_OFFSET_LIMIT);
 }
 
--- Source/HexGame/Public/HGTileActor.h
+++ Source/HexGame/Public/HGTileActor.h
@@ -60,8 +60,13 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+	virtual float GetRandomZOffset();
 
+public:
+	FAxialCoordinate HexCoord;
+
+	UPROPERTY(EditAnywhere)
+	UStaticMeshComponent* MeshComponent;
+	
+	FAxialCoordinate const GetHexCoordinate();
 };
